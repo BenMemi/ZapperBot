7ccb948acf7920f656e04650dc5e6a25c10af7ed
Joshua <joshua.steele@mycelium.ventures>
2021-10-19 20:52:33 +1030

    Blursed client

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-function main() {
+const axios = require('axios');
+
+const getUserQuests = (userId) => 
+    axios.get(`https://zapper.fi/_next/data/${userId}/en/quests.json`);
+
+const getUserRewards = (userId) =>
+    axios.get(`https://zapper.fi/_next/data/${userId}/en/rewards.json`);
+
+const main = () => {
     console.log("Blursed ZapperBot");
 }
 
