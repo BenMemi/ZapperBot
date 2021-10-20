b99c2a9839f9d34e39c78f3101976d0a12dc3bea
Joshua <joshua.steele@mycelium.ventures>
2021-10-21 09:34:48 +1030

    Blursed bot
    Co-authored-by: Matt Durkin <m.durkin92@gmail.com>
    Co-authored-by: BenM-Mycelium <BenM-Mycelium@users.noreply.github.com>
    Co-authored-by: abellliam <abellliam@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const axios = require('axios');
+const axios = require('axios').default;
 
-const getUserQuests = (userId) => 
-    axios.get(`https://zapper.fi/_next/data/${userId}/en/quests.json`);
 
-const getUserRewards = (userId) =>
-    axios.get(`https://zapper.fi/_next/data/${userId}/en/rewards.json`);
 
-const main = () => {
-    console.log("Blursed ZapperBot");
+const getUserQuests = async (userId) => 
+    (await axios.get(`https://zapper.fi/_next/data/${userId}/en/quests.json`)).data;
+
+const getUserRewards = async (userId) =>
+    (await axios.get(`https://zapper.fi/_next/data/${userId}/en/rewards.json`)).data;
+
+const main = async () => {
+    const userId = process.argv[2];
+    const userQuests =  await getUserQuests(userId);
+    const userRewards = await getUserRewards(userId);
+    console.log(userQuests);
+    console.log(userRewards);
+    console.log("Blursed ZapperBot"); 
 }
 
 main();
